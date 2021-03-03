be7fcb13d55bbd2ed84664b47e35c3e70d11b45d
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-03-03 10:00:32 -0800

    Changed Assertions import

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -3,24 +3,26 @@ package persistence;
 import model.stock.BoringStock;
 import model.stock.MemeStock;
 import model.stock.Stock;
+import org.junit.Assert;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.Assert.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 //REFERENCE: modelled on JsonReaderTest class in JsonSerializationDemo project
 public class JsonTest {
     protected void checkStock(String type, String tkr, double val, Stock stock) {
         if (stock.getType().equals("BoringStock")) {
             //check equality of arguments + type should be BoringStock
-            assertTrue(type.equals("BoringStock"));
+            Assert.assertEquals("BoringStock", type);
             assertTrue(stock instanceof BoringStock);
             assertEquals(tkr, stock.getStockTicker());
             assertEquals(val, stock.getCurrentValueDouble());
 
         } else {
             //check equality of arguments + type should be MemeStock
-            assertTrue(type.equals("MemeStock"));
+            Assert.assertEquals("MemeStock", type);
             assertTrue(stock instanceof MemeStock);
             assertEquals(tkr, stock.getStockTicker());
             assertEquals(val, stock.getCurrentValueDouble());
