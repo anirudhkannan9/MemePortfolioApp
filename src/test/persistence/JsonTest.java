1ef1d276b36a845e7983522f9c9053bfcd4ab1bc
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-03-03 10:01:59 -0800

    Changed Assertions import

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -3,10 +3,6 @@ package persistence;
 import model.stock.BoringStock;
 import model.stock.MemeStock;
 import model.stock.Stock;
-import org.junit.Assert;
-
-//import static org.junit.Assert.assertTrue;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,14 +11,14 @@ public class JsonTest {
     protected void checkStock(String type, String tkr, double val, Stock stock) {
         if (stock.getType().equals("BoringStock")) {
             //check equality of arguments + type should be BoringStock
-            Assert.assertEquals("BoringStock", type);
+            assertEquals("BoringStock", type);
             assertTrue(stock instanceof BoringStock);
             assertEquals(tkr, stock.getStockTicker());
             assertEquals(val, stock.getCurrentValueDouble());
 
         } else {
             //check equality of arguments + type should be MemeStock
-            Assert.assertEquals("MemeStock", type);
+            assertEquals("MemeStock", type);
             assertTrue(stock instanceof MemeStock);
             assertEquals(tkr, stock.getStockTicker());
             assertEquals(val, stock.getCurrentValueDouble());
