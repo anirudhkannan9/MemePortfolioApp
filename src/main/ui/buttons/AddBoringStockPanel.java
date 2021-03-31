2e416ecc91b43cff41b9fcbbbbcae0d687416c1a
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-03-31 10:12:05 -0700

    Added unversioned files.

---
+++ src/main/ui/buttons/AddBoringStockPanel.java
@@ -0,0 +1,84 @@
+package ui.buttons;
+
+import model.portfolio.Portfolio;
+import model.stock.BoringStock;
+import ui.MemePortfolioAppGUI;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import static java.lang.Double.parseDouble;
+
+public class AddBoringStockPanel extends JPanel {
+
+    public AddBoringStockPanel(MemePortfolioAppGUI memePortfolioAppGUI, Portfolio p) {
+        Dimension size = getPreferredSize();
+        size.width = 250;
+        setPreferredSize(size);
+
+        setBorder(BorderFactory.createTitledBorder("Create a boring stock to add to your portfolio"));
+
+        JLabel tickerLabel = new JLabel("Stock Ticker: ");
+        JLabel priceLabel = new JLabel("Stock price: $");
+        JTextField tickerField = new JTextField(10);
+        JTextField priceField = new JTextField(10);
+        JButton addBtn = new JButton("Add to Portfolio");
+        addBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                playSound("./button.wav");
+                p.addStock(new BoringStock(tickerField.getText(), parseDouble(priceField.getText())));
+                memePortfolioAppGUI.repaint();
+            }
+        });
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+
+        //First column:
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.weightx = 0.5;
+        gc.weighty = 0.5;
+        gc.gridx = 0;
+        gc.gridy = 0;
+        add(tickerLabel, gc);
+        gc.gridx = 0;
+        gc.gridy = 1;
+        add(priceLabel, gc);
+
+        //Second column
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.gridx = 1;
+        gc.gridy = 0;
+        add(tickerField, gc);
+        gc.gridx = 1;
+        gc.gridy = 1;
+        add(priceField, gc);
+
+        //final row
+        gc.weighty = 10;
+        gc.anchor = GridBagConstraints.FIRST_LINE_END;
+        gc.gridx = 1;
+        gc.gridy = 2;
+        add(addBtn, gc);
+    }
+
+    //EFFECTS: plays sound based on path to audio file passed in
+    public void playSound(String soundName) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception e) {
+            System.out.println("Error with playing sound.");
+            e.printStackTrace();
+        }
+    }
+}
